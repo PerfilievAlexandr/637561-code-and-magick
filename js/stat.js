8698544b76985afdd0843c271c00da8cbdff9f14
Alexender Perfiliev <perfiliev29@yandex.ru>
2018-06-04 23:14:47 +0300

    Выражения добавлены в переменные и добавлены функции

--- js/stat.js
+++ js/stat.js
@@ -10,21 +10,24 @@ var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var BETWEEN_COLUMN = 50;
 
-
 var renderCloud = function (ctx, CLOUD_X1, CLOUD_Y1, color) {
-  var radiusBegin = 50;
-  var radiusMaxCircle = 10;
+  var RADIUS_BEGIN = 50;
+  var RADIUS_MAX_CIRCLE = 10;
+  var indentCloudWidth = CLOUD_X1 + CLOUD_WIDTH;
+  var indentCloudHeidht = CLOUD_Y1 + CLOUD_HEIGHT;
+  var startRadiusX = CLOUD_X1 + CLOUD_WIDTH - RADIUS_BEGIN;
+  var startRadiusY = CLOUD_Y1 + CLOUD_HEIGHT - RADIUS_BEGIN;
 
   ctx.fillStyle = color;
 
+  ctx.beginPath();
   ctx.moveTo(CLOUD_X1, CLOUD_Y1);
-  ctx.lineTo(CLOUD_X1 + CLOUD_WIDTH - radiusBegin - radiusMaxCircle, CLOUD_Y1);
-  ctx.bezierCurveTo(CLOUD_X1 + CLOUD_WIDTH - radiusBegin, CLOUD_Y1, CLOUD_X1 + CLOUD_WIDTH - radiusMaxCircle, CLOUD_Y1 + radiusMaxCircle, CLOUD_X1 + CLOUD_WIDTH, CLOUD_Y1 + radiusBegin);
-  ctx.lineTo(CLOUD_X1 + CLOUD_WIDTH, CLOUD_Y1 + CLOUD_HEIGHT - radiusBegin - radiusMaxCircle);
-  ctx.bezierCurveTo(CLOUD_X1 + CLOUD_WIDTH, CLOUD_Y1 + CLOUD_HEIGHT - radiusBegin, CLOUD_X1 + CLOUD_WIDTH - radiusMaxCircle, CLOUD_Y1 + CLOUD_HEIGHT - radiusMaxCircle, CLOUD_X1 + CLOUD_WIDTH - radiusBegin, CLOUD_Y1 + CLOUD_HEIGHT);
-  ctx.lineTo(CLOUD_X1, CLOUD_Y1 + CLOUD_HEIGHT);
+  ctx.lineTo(startRadiusX - RADIUS_MAX_CIRCLE, CLOUD_Y1);
+  ctx.bezierCurveTo(startRadiusX, CLOUD_Y1, indentCloudWidth - RADIUS_MAX_CIRCLE, CLOUD_Y1 + RADIUS_MAX_CIRCLE, indentCloudWidth, CLOUD_Y1 + RADIUS_BEGIN);
+  ctx.lineTo(indentCloudWidth, startRadiusY - RADIUS_MAX_CIRCLE);
+  ctx.bezierCurveTo(indentCloudWidth, startRadiusY, indentCloudWidth - RADIUS_MAX_CIRCLE, indentCloudHeidht - RADIUS_MAX_CIRCLE, startRadiusX, indentCloudHeidht);
+  ctx.lineTo(CLOUD_X1, indentCloudHeidht);
   ctx.closePath();
-  ctx.stroke();
   ctx.fill();
 };
 
@@ -39,28 +42,35 @@ var getMaxTime = function (arr) {
   return maxTime;
 };
 
+var getRandomColorRGBA = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
 window.renderStatistics = function (ctx, players, times) {
 
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  var indentTextX = CLOUD_X + GAP;
+  var indentTextY = CLOUD_Y + GAP;
+  var columnIndent = BETWEEN_COLUMN + BAR_WIDTH;
+
+  renderCloud(ctx, indentTextX, indentTextY, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + (GAP + FONT_GAP) * 2);
+  ctx.fillText('Ура вы победили!', indentTextX, indentTextY + FONT_GAP);
+  ctx.fillText('Список результатов:', indentTextX, CLOUD_Y + (GAP + FONT_GAP) * 2);
 
   var hieghestColomn = getMaxTime(times);
 
   for (var i = 0; i < players.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(players[i], CLOUD_X + GAP + (BETWEEN_COLUMN + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
-    ctx.fillText(Math.floor(times[i]), CLOUD_X + GAP + (BETWEEN_COLUMN + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP * 2 - FONT_GAP - (BAR_HEIGHT * times[i]) / hieghestColomn);
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(' + (Math.floor(Math.random() * (255 - 1 + 1)) + 1) + ', ' + (Math.floor(Math.random() * (255 - 1 + 1)) + 1) + ', ' + 255 + ', ' + 1 + ')';
-    }
+    var textLocationColumnX = indentTextX + columnIndent * i;
+    var textLocationColumnY = CLOUD_Y + CLOUD_HEIGHT - GAP;
+    var proportionColumns = BAR_HEIGHT * times[i] / hieghestColomn;
 
-    ctx.fillRect(CLOUD_X + GAP + (BETWEEN_COLUMN + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP, BAR_WIDTH, -(BAR_HEIGHT * times[i]) / hieghestColomn);
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], textLocationColumnX, textLocationColumnY);
+    ctx.fillText(Math.floor(times[i]), indentTextX + columnIndent * i, textLocationColumnY - GAP - FONT_GAP - proportionColumns);
+    ctx.fillStyle = (players[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(' + getRandomColorRGBA(1, 255) + ', ' + getRandomColorRGBA(1, 255) + ', ' + 255 + ', ' + 1 + ')';
+    ctx.fillRect(textLocationColumnX, textLocationColumnY - FONT_GAP, BAR_WIDTH, -proportionColumns);
   }
 };
